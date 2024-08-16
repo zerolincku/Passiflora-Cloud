e4f384dde5c5e68adafa7b9e83b8aeb41044bea8
林常坤 <zerolinck@foxmail.com>
2024-08-16 09:56:01 +0800

    style: update

---
+++ passiflora-server/modules/passiflora-feign/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java-library")
+}
+
+configurations.all {
+    exclude("org.springframework.boot", "spring-boot-starter-logging")
+}
+
+dependencies {
+    api(project(":modules:passiflora-common"))
+    annotationProcessor(platform(project(":modules:passiflora-bom")))
+    testAnnotationProcessor(platform(project(":modules:passiflora-bom")))
+
+    annotationProcessor("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.mapstruct:mapstruct-processor")
+    testAnnotationProcessor("org.mapstruct:mapstruct-processor")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding")
+    testAnnotationProcessor("org.projectlombok:lombok-mapstruct-binding")
+
+    api("org.springframework.cloud:spring-cloud-openfeign-core")
+    api("io.swagger.core.v3:swagger-annotations-jakarta")
+    api("cn.hutool:hutool-extra")
+    api("io.github.openfeign:feign-core")
+    implementation("org.apache.tomcat.embed:tomcat-embed-core")
+}
