32681cca8136a6d2dc32f8786dda793cfaafed06
林常坤 <zerolinck@foxmail.com>
2024-12-20 19:30:53 +0800

    移除 mybatis xml 文件

--- passiflora-server/passiflora-iam-app/src/main/java/com/zerolinck/passiflora/iam/mapper/IamUserPositionMapper.java
+++ passiflora-server/passiflora-iam-app/src/main/java/com/zerolinck/passiflora/iam/mapper/IamUserPositionMapper.java
@@ -16,36 +16,83 @@
  */
 package com.zerolinck.passiflora.iam.mapper;
 
-import java.util.Collection;
-import java.util.List;
-
-import org.apache.ibatis.annotations.Param;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zerolinck.passiflora.model.iam.entity.IamUserPosition;
 import com.zerolinck.passiflora.model.iam.resp.IamUserPositionResp;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.ibatis.annotations.Param;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 /** @author linck on 2024-05-14 */
 public interface IamUserPositionMapper extends BaseMapper<IamUserPosition> {
 
-    @NotNull List<IamUserPositionResp> selectByUserIds(@NotNull @Param("userIds") Collection<String> userIds);
+    @NotNull
+    List<IamUserPositionResp> selectByUserIds(@NotNull @Param("userIds") Collection<String> userIds);
 
-    /** 真实删除 */
-    int deleteByIds(@NotNull @Param("ids") Collection<String> ids, @NotNull @Param("updateBy") String updateBy);
+    default int deleteByIds(@NotNull @Param("ids") Collection<String> ids, @NotNull @Param("updateBy") String updateBy) {
+        if (CollectionUtils.isEmpty(ids)) {
+            return 0;
+        }
 
-    /** 真实删除 */
-    int deleteByUserIds(
-            @NotNull @Param("userIds") Collection<String> userIds, @Nullable @Param("updateBy") String updateBy);
+        LambdaUpdateWrapper<IamUserPosition> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper
+                .in(IamUserPosition::getId, ids)
+                .set(IamUserPosition::getUpdateTime, LocalDateTime.now())
+                .set(IamUserPosition::getUpdateBy, updateBy)
+                .set(IamUserPosition::getDelFlag, 1);
 
-    /** 真实删除 */
-    int deleteByPositionIds(
-            @NotNull @Param("positionIds") Collection<String> positionIds,
-            @Nullable @Param("updateBy") String updateBy);
+        return this.update(null, updateWrapper);
+    }
 
-    /** 真实删除 */
-    int deleteByUserIdAndPositionIds(
-            @NotNull @Param("userId") String userId,
-            @NotNull @Param("positionIds") Collection<String> positionIds,
-            @Nullable @Param("updateBy") String updateBy);
+    default int deleteByUserIds(@NotNull @Param("userIds") Collection<String> userIds, @Nullable @Param("updateBy") String updateBy) {
+        if (CollectionUtils.isEmpty(userIds)) {
+            return 0;
+        }
+
+        LambdaUpdateWrapper<IamUserPosition> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper
+                .in(IamUserPosition::getUserId, userIds)
+                .set(IamUserPosition::getUpdateTime, LocalDateTime.now())
+                .set(IamUserPosition::getUpdateBy, updateBy)
+                .set(IamUserPosition::getDelFlag, 1);
+
+        return this.update(null, updateWrapper);
+    }
+
+    default int deleteByPositionIds(@NotNull @Param("positionIds") Collection<String> positionIds, @Nullable @Param("updateBy") String updateBy) {
+        if (CollectionUtils.isEmpty(positionIds)) {
+            return 0;
+        }
+
+        LambdaUpdateWrapper<IamUserPosition> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper
+                .in(IamUserPosition::getPositionId, positionIds)
+                .set(IamUserPosition::getUpdateTime, LocalDateTime.now())
+                .set(IamUserPosition::getUpdateBy, updateBy)
+                .set(IamUserPosition::getDelFlag, 1);
+
+        return this.update(null, updateWrapper);
+    }
+
+    default int deleteByUserIdAndPositionIds(@NotNull @Param("userId") String userId, @NotNull @Param("positionIds") Collection<String> positionIds, @Nullable @Param("updateBy") String updateBy) {
+        if (CollectionUtils.isEmpty(positionIds)) {
+            return 0;
+        }
+
+        LambdaUpdateWrapper<IamUserPosition> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper
+                .eq(IamUserPosition::getUserId, userId)
+                .in(IamUserPosition::getPositionId, positionIds)
+                .set(IamUserPosition::getUpdateTime, LocalDateTime.now())
+                .set(IamUserPosition::getUpdateBy, updateBy)
+                .set(IamUserPosition::getDelFlag, 1);
+
+        return this.update(null, updateWrapper);
+    }
 }
