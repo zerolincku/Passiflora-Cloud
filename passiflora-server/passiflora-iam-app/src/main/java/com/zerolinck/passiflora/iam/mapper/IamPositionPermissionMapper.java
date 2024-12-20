6e5f227d9769e1a15218a496d789b404e01e5a68
林常坤 <zerolinck@foxmail.com>
2024-12-20 19:24:53 +0800

    移除 mybatis xml 文件

--- passiflora-server/passiflora-iam-app/src/main/java/com/zerolinck/passiflora/iam/mapper/IamPositionPermissionMapper.java
+++ passiflora-server/passiflora-iam-app/src/main/java/com/zerolinck/passiflora/iam/mapper/IamPositionPermissionMapper.java
@@ -16,13 +16,17 @@
  */
 package com.zerolinck.passiflora.iam.mapper;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
@@ -31,18 +35,67 @@ import com.zerolinck.passiflora.model.iam.entity.IamPositionPermission;
 
 /** @author linck on 2024-05-06 */
 public interface IamPositionPermissionMapper extends BaseMapper<IamPositionPermission> {
-    Page<IamPositionPermission> page(
-            IPage<IamPositionPermission> page,
+
+    @NotNull default Page<IamPositionPermission> page(
+            @NotNull IPage<IamPositionPermission> page,
             @Param(Constants.WRAPPER) QueryWrapper<IamPositionPermission> searchWrapper,
-            @Param("sortWrapper") QueryWrapper<IamPositionPermission> sortWrapper);
+            @Param("sortWrapper") QueryWrapper<IamPositionPermission> sortWrapper) {
+        if (searchWrapper == null) {
+            searchWrapper = new QueryWrapper<>();
+        }
+        searchWrapper.eq("del_flag", 0);
 
-    /** 真实删除 */
-    int deleteByIds(@NotNull @Param("ids") Collection<String> ids, @Nullable @Param("updateBy") String updateBy);
+        if (sortWrapper == null
+                || sortWrapper.getSqlSegment() == null
+                || sortWrapper.getSqlSegment().isEmpty()) {
+            searchWrapper.orderByAsc("id");
+        } else {
+            searchWrapper.last(sortWrapper.getSqlSegment());
+        }
 
-    /** 真实删除 */
-    int deleteByPositionIds(
-            @NotNull @Param("positionIds") Collection<String> positionIds,
-            @Nullable @Param("updateBy") String updateBy);
+        return (Page<IamPositionPermission>) this.selectPage(page, searchWrapper);
+    }
 
-    @NotNull List<String> permissionIdsByPositionIds(@NotNull @Param("positionIds") List<String> positionIds);
+    default int deleteByIds(
+            @Nullable @Param("ids") Collection<String> ids, @Nullable @Param("updateBy") String updateBy) {
+        if (CollectionUtils.isEmpty(ids)) {
+            return 0;
+        }
+
+        LambdaUpdateWrapper<IamPositionPermission> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper
+                .in(IamPositionPermission::getId, ids)
+                .set(IamPositionPermission::getUpdateTime, LocalDateTime.now())
+                .set(IamPositionPermission::getUpdateBy, updateBy)
+                .set(IamPositionPermission::getDelFlag, 1);
+
+        return this.update(null, updateWrapper);
+    }
+
+    default int deleteByPositionIds(
+            @Nullable @Param("positionIds") Collection<String> positionIds,
+            @Nullable @Param("updateBy") String updateBy) {
+        if (CollectionUtils.isEmpty(positionIds)) {
+            return 0;
+        }
+        LambdaUpdateWrapper<IamPositionPermission> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper
+                .in(IamPositionPermission::getPositionId, positionIds)
+                .set(IamPositionPermission::getUpdateTime, LocalDateTime.now())
+                .set(IamPositionPermission::getUpdateBy, updateBy)
+                .set(IamPositionPermission::getDelFlag, 1);
+
+        return this.update(null, updateWrapper);
+    }
+
+    @NotNull @Select(
+            """
+        SELECT permission_id from iam_permission
+        WHERE permission_status = 1 AND del_flag = 0
+        AND permission_id IN
+            (SELECT permission_id from iam_position_permission
+            WHERE del_flag = 0 AND position_id IN
+            (SELECT position_id from iam_position_permission
+            WHERE del_flag = 0 AND position_id IN (#{positionIds})))""")
+    List<String> permissionIdsByPositionIds(@NotNull @Param("positionIds") List<String> positionIds);
 }
